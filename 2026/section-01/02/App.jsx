536d04e8e2e1a36069e0b4c34db687e882f17125
felipe.magalhaes <ffelipemagallhaes@gmail.com>
2026-04-26 12:09:14 -0400

    Implement Advice app

--- 2026/section-01/02/App.jsx
+++ 2026/section-01/02/App.jsx
@@ -1,8 +1,33 @@
+import { useEffect, useState } from "react";
+
 export default function App() {
+  const [advice, setAdvice] = useState("");
+  const [counter, setCounter] = useState(0);
+  console.log("⬜ - App - advice:", advice);
+
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data?.slip?.advice);
+    setCounter((prev) => prev + 1);
+  }
+
+  useEffect(function () {
+    getAdvice();
+  }, []);
+
   return (
     <div>
-      <h1>Hello World!</h1>
-      <button>Get advice</button>
+      <h1>Advice:</h1>
+      <div>
+        <h1>{advice || ""}</h1>
+        <Message counter={counter} />
+      </div>
+      <button onClick={getAdvice}>Get advice</button>
     </div>
   );
+}
+
+function Message(props) {
+  return <p>You have read {props.counter} advice(s)</p>;
 }
